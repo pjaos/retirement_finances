a8dabdf1fa9a50931bc16a2b65a6e099d17f49e6
Paul Austen <pjaos@gmail.com>
2025-03-08 19:22:02 +0000

    Add separate Linux and Windows installer folders.

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Ensure we remove old installer versions.
 rm -rf dist
+rm -f installers/linux/*.whl
+rm -f installers/windows/*.whl
 set -e
 # syntax checking
 pyflakes3 retirement_finances/*.py
 # code style checking
 pycodestyle --max-line-length=250 retirement_finances/*.py
 poetry -vvv build
-cp dist/*.whl installers
+cp dist/*.whl installers/linux
+cp dist/*.whl installers/windows
 
 
