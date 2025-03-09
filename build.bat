85413a31ee5345d175cb40c7e648b14c10bf21ae
Paul Austen <pausten.os@gmail.com>
2025-03-09 12:00:21 +0000

    Ensure files builkt replace oild files.

--- build.bat
+++ build.bat
@@ -1,9 +1,11 @@
 REM build python wheel on a windows platform
-del installers/linux/*.whl
-del installers/windows/*.whl
+del dist\*.whl
+del dist\*.tar.gz
+del installers\linux\*.whl
+del installers\windows\*.whl
 poetry -vvv build
-copy dist/*.whl installers/linux
-copy dist/*.whl installers/windows
+copy dist\*.whl installers\linux
+copy dist\*.whl installers\windows
 
 
 
