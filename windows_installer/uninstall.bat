8fbf0a13320618a009fc9757127d2f27dd042925
Paul Austen <pausten.os@gmail.com>
2025-04-13 08:25:19 +0100

    - Check for installed python.org python - Ensure uninstall.bat removes folder

--- windows_installer/uninstall.bat
+++ windows_installer/uninstall.bat
@@ -1,10 +1,6 @@
 REM Remove the files created during installation
 
-REM We don't check for errors on the removal of this folder as the __pycache__ folder
-REM will only be present in it if the application was launched.
-rd /S /Q retirement_finances
-
-python -m poetry env remove --all
+poetry env remove --all
 if  errorlevel 1 goto CMD_ERROR
 
 del get-pip.py
@@ -12,6 +8,10 @@ if  errorlevel 1 goto CMD_ERROR
 
 del poetry.lock
 if  errorlevel 1 goto CMD_ERROR
+
+REM We don't check for errors on the removal of this folder as the __pycache__ folder
+REM will only be present in it if the application was launched.
+rd /S /Q retirement_finances
 
 exit /b 0
 
