32509771f68912caf7b93c373b743b9682b1b0c7
Paul Austen <pjaos@gmail.com>
2025-06-26 21:18:42 +0100

    Add 4.9 linux installers

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 # Ensure we remove old installer versions.
 rm -rf dist
 rm -f installers/linux/*.whl
-rm -f installers/windows/*.whl
 set -e
 # syntax checking
 pyflakes3 retirement_finances/*.py
@@ -10,7 +9,6 @@ pyflakes3 retirement_finances/*.py
 pycodestyle --max-line-length=250 retirement_finances/*.py
 poetry -vvv build
 cp dist/*.whl installers/linux
-cp dist/*.whl installers/windows
 # Generate new docx readme file
 ./create_single_docx_readme.sh
 
