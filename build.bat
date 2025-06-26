5c2ae319630e0e228d4c7c9af41046d6221a4da4
Paul Austen <pjaos@gmail.com>
2025-06-26 21:19:35 +0100

    Don't alter the Linux installer files when building the windows installer.

--- build.bat
+++ build.bat
@@ -1,10 +1,8 @@
 REM build python wheel on a windows platform
 del dist\*.whl
 del dist\*.tar.gz
-del installers\linux\*.whl
 del installers\windows\*.whl
 poetry -vvv build
-copy dist\*.whl installers\linux
 copy dist\*.whl installers\windows
 
 
