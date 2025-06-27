bd38dbe73b8ad7e8373c029265f2194fd4d37295
Paul Austen <pausten.os@gmail.com>
2025-06-27 19:31:58 +0100

    Add 4.9 Windows installer

--- build.bat
+++ build.bat
@@ -1,12 +1,5 @@
-REM build python wheel on a windows platform
-REM del dist\*.whl
-REM del dist\*.tar.gz
-REM del installers\windows\*.whl
-REM poetry -vvv build
-REM copy dist\*.whl installers\windows
-
-del /Q windows_installer\windows\*.exe
-del /Q  windows_installer/*.exe
+del /Q windows_installer\*.exe
+del /Q installers\windows\*.exe
 pyinstaller --distpath windows_installer/ --workpath windows_installer/ retirement_finances.spec
 "C:\Program Files (x86)\Inno Setup 6\iscc.exe" windows_installer/retirement_finances.iss
 
