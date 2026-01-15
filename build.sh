12a20309c74467d57e0fc506d95dd52b05220e61
Paul Austen <pjaos@gmail.com>
2026-01-15 10:10:10 +0000

    Handle ne location or retirement_finances folder in src folder

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ rm -rf dist
 rm -f installers/linux/*.whl
 set -e
 # syntax checking
-pyflakes3 retirement_finances/*.py
+pyflakes3 src/retirement_finances/*.py
 # code style checking
-pycodestyle --max-line-length=300 retirement_finances/*.py
+pycodestyle --max-line-length=300 src/retirement_finances/*.py
+cp pyproject.toml src/retirement_finances/assets
 poetry -vvv build
 cp dist/*.whl installers/linux
 # Generate new docx readme file
