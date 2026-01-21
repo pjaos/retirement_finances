419f1dd499ef7b59106cdffc366e3b957d7391e3
Paul Austen <pausten.os@gmail.com>
2026-01-21 16:15:45 +0000

    Ensure tar.gz file is deleted.

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ copy .\pyproject.toml .\src\retirement_finances\assets\
 :: Use poetry command to build python wheel
 poetry --output=windows --clean -vvv build
 :: Delete the .tar.gz file in dist directory
-del /Q windows/*.tar.gz
+del /Q windows\retirement_finances-*.tar.gz
 :: Put a copy of the install.py alongside the python wheel
 copy install.py windows
 
