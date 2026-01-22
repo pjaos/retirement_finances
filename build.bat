ce7ac8b50fcc3420af393fe1fdab81fcf041a82d
Paul Austen <pausten.os@gmail.com>
2026-01-22 15:21:13 +0000

    When building the wndows python wheel place the output in the installers/windows folder.

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 :: This builds the python wheel file in the windows folder that can be installed using the install.py file.
 copy .\pyproject.toml .\src\retirement_finances\assets\
 :: Use poetry command to build python wheel
-poetry --output=windows --clean -vvv build
+poetry --output=installers\windows --clean -vvv build
 :: Delete the .tar.gz file in dist directory
-del /Q windows\retirement_finances-*.tar.gz
+del /Q .\installers\windows\retirement_finances-*.tar.gz
 :: Put a copy of the install.py alongside the python wheel
-copy install.py windows
+copy .\install.py .\installers\windows\
 
 
 
